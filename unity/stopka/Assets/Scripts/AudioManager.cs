0756452bdd41b60d8915c7e4345c4af5b67e8991
Pavel Dushkov <velixor.dev@gmail.com>
2026-03-15 19:39:14 +0200

    chore: add stub classes for GameUI, AudioManager, CameraShake

---
+++ unity/stopka/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Stopka
+{
+    public class AudioManager : MonoBehaviour
+    {
+    }
+}
---
+++ unity/stopka/Assets/Scripts/GameUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Stopka
+{
+    public class GameUI : MonoBehaviour
+    {
+        public void SetState(GameState state, ScoreManager scoreManager)
+        {
+        }
+
+        public void UpdateScore(ScoreManager scoreManager)
+        {
+        }
+    }
+}
