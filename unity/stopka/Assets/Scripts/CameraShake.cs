662cfb2fd61242dd1fb678b870b33856bbfcc71a
Pavel Dushkov <velixor.dev@gmail.com>
2026-03-15 19:45:02 +0200

    feat: add CameraShake for combo screen shake

--- unity/stopka/Assets/Scripts/CameraShake.cs
+++ unity/stopka/Assets/Scripts/CameraShake.cs
@@ -1,8 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Stopka
 {
     public class CameraShake : MonoBehaviour
     {
+        public void Shake(float intensity = 0.1f, float duration = 0.15f)
+        {
+            StopAllCoroutines();
+            StartCoroutine(ShakeCoroutine(intensity, duration));
+        }
+
+        private IEnumerator ShakeCoroutine(float intensity, float duration)
+        {
+            Vector3 originalLocalPos = transform.localPosition;
+            float elapsed = 0f;
+
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+                float strength = intensity * (1f - elapsed / duration);
+                transform.localPosition = originalLocalPos + Random.insideUnitSphere * strength;
+                yield return null;
+            }
+
+            transform.localPosition = originalLocalPos;
+        }
     }
 }
